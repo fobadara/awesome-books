c9504cedab9066ef37f89605facf3cfaa0b3f9d4
fobadara <62938735+fobadara@users.noreply.github.com>
2021-09-16 12:09:50 +0100

    Refactor code to use classes and objects

--- javascript/index.js
+++ javascript/index.js
@@ -1,73 +1,76 @@
-function Library() {
-  this.books = JSON.parse(localStorage.getItem('books'));
-  this.storage = localStorage.setItem('books', JSON.stringify(this.books));
-  this.storedBooks = JSON.parse(localStorage.getItem('books'));
-}
-const library = new Library();
+class Library{
+  constructor(){
+    this.books = JSON.parse(localStorage.getItem('books'));
+    this.storage = localStorage.setItem('books', JSON.stringify(this.books));
+    this.storedBooks = JSON.parse(localStorage.getItem('books'));
+  }
 
-function RemoveBook() {
-  for (let index = 0; index < library.storedBooks.length; index += 1) {
-    if ((library.storedBooks[i].parentElement.children[0].innerText === library.storedBooks[index].title)
-      && (library.storedBooks[i].parentElement.children[1].innerText === library.storedBooks[index].author)) {
-      library.storedBooks.splice(index, 1);
-      localStorage.setItem('books', JSON.stringify(library.storedBooks));
-      library.storedBooks = JSON.parse(localStorage.getItem('books'));
-      window.location.reload();
+  removeBook(event) {
+    for (let index = 0; index < library.storedBooks.length; index += 1) {
+      if ((event.target.parentElement.children[0].innerText === library.storedBooks[index].title)
+        && (event.target.parentElement.children[1].innerText === library.storedBooks[index].author)) {
+        library.storedBooks.splice(index, 1);
+        localStorage.setItem('books', JSON.stringify(library.storedBooks));
+        library.storedBooks = JSON.parse(localStorage.getItem('books'));
+        window.location.reload();
+      }
     }
   }
-}
-const remove = new RemoveBook();
-
-function RenderBooks() {
-  for (let i = 0; i < library.storedBooks.length; i += 1) {
-    this.p = library.storedBooks[i];
-    this.div = document.createElement('div');
-    this.div.className = i;
-    this.para1 = document.createElement('p');
-    para1.innerHTML = this.p.title;
-    div.appendChild(para1);
-    this.para2 = document.createElement('p');
-    para2.innerHTML = this.p.author;
-    div.appendChild(para2);
-    this.container = document.getElementById('container');
-    this.container.appendChild(div);
-    this.removeBtn = document.createElement('button');
-    this.removeBtn.id = i;
-    this.removeBtn.innerText = 'Remove';
-    this.div.appendChild(removeBtn);
-    this.removeBtn.addEventListener('click', this.removeBook);
-    this.hr = document.createElement('hr');
-    this.div.appendChild(hr);
+  
+  renderBooks() {
+    for (let i = 0; i < library.storedBooks.length; i += 1) {
+      this.p = library.storedBooks[i];
+      this.div = document.createElement('div');
+      this.div.className = i;
+      this.para1 = document.createElement('p');
+      this.para1.innerHTML = this.p.title;
+      this.div.appendChild(this.para1);
+      this.para2 = document.createElement('p');
+      this.para2.innerHTML = this.p.author;
+      this.div.appendChild(this.para2);
+      this.container = document.getElementById('container');
+      this.container.appendChild(this.div);
+      this.removeBtn = document.createElement('button');
+      this.removeBtn.id = i;
+      this.removeBtn.innerText = 'Remove';
+      this.div.appendChild(this.removeBtn);
+      this.removeBtn.addEventListener('click', library.removeBook);
+      this.hr = document.createElement('hr');
+      this.div.appendChild(this.hr);
+    }
+  }
+  
+  checkRender() {
+    if (library.storedBooks !== null) {
+      this.renderBooks();
+    } else {
+      library.storedBooks = [];
+      library.books = [];
+      this.renderBooks();
+    }  
+  }
+  
+  addBook() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const book = { title, author };
+    library.storedBooks = library.storedBooks.filter((book)=> {
+      return book.title !== title || book.author !== author;
+    })
+    library.storedBooks.push(book);
+    library.books.push(book);
+    console.log(title);
+    console.log(library.storedBooks);
+    localStorage.setItem('books', JSON.stringify(library.storedBooks));
+    library.storedBooks = JSON.parse(localStorage.getItem('books'));
+    window.location.reload();
+  }
+  addEvent(){
+    document.getElementById('btn').addEventListener('click', library.addBook);
   }
 }
 
-function CheckRender() {
-  if (library.storedBooks !== null) {
-    const render = new RenderBooks();
-  } else {
-    library.storedBooks = [];
-    books = [];
-    const render = new RenderBooks();
-  }  
-}
-
-// function addBook() {
-//   const title = document.getElementById('title').value;
-//   const author = document.getElementById('author').value;
-//   const book = { title, author };
-//   library.storedBooks = library.storedBooks.filter((book)=> {
-//     return book.title !== title || book.author !== author;
-//   })
-//   library.storedBooks.push(book);
-//   books.push(book);
-//   console.log(title);
-//   console.log(library.storedBooks);
-//   localStorage.setItem('books', JSON.stringify(library.storedBooks));
-//   library.storedBooks = JSON.parse(localStorage.getItem('books'));
-//   window.location.reload();
-//   document.getElementById('title').value = '';
-//   document.getElementById('author').value = '';
-// }
-// document.getElementById('btn').addEventListener('click', addBook);
-
-// }
+const library = new Library();
+library.addEvent;
+library.checkRender();
+library.addEvent();
