0dcfe2560924d89bc254f0c0469698b86d388a52
fobadara <62938735+fobadara@users.noreply.github.com>
2021-09-17 16:01:49 +0100

    Add style

--- javascript/index.js
+++ javascript/index.js
@@ -5,8 +5,7 @@ let storedBooks = JSON.parse(localStorage.getItem('books'));
 class Library {
   removeBook = (event) => {
     for (let index = 0; index < storedBooks.length; index += 1) {
-      if ((event.target.parentElement.children[0].innerText === storedBooks[index].title)
-        && (event.target.parentElement.children[1].innerText === storedBooks[index].author)) {
+      if (event.target.id === `${index}`) {
         storedBooks.splice(index, 1);
         localStorage.setItem('books', JSON.stringify(storedBooks));
         storedBooks = JSON.parse(localStorage.getItem('books'));
@@ -17,24 +16,29 @@ class Library {
 
   renderBooks() {
     for (let i = 0; i < storedBooks.length; i += 1) {
+      // Create elements
+      this.fragment = new DocumentFragment();
       this.p = storedBooks[i];
-      this.div = document.createElement('div');
-      this.div.className = i;
-      this.para1 = document.createElement('p');
-      this.para1.innerHTML = this.p.title;
-      this.div.appendChild(this.para1);
-      this.para2 = document.createElement('p');
-      this.para2.innerHTML = this.p.author;
-      this.div.appendChild(this.para2);
-      this.container = document.getElementById('container');
-      this.container.appendChild(this.div);
+      this.ul = document.createElement('ul');
+      this.content = document.createElement('li');
+      this.span = document.createElement('span')
+      this.span.innerHTML = `${i + 1}. "${this.p.title}" by "${this.p.author}"`
       this.removeBtn = document.createElement('button');
       this.removeBtn.id = i;
-      this.removeBtn.innerText = 'Remove';
-      this.div.appendChild(this.removeBtn);
-      this.removeBtn.addEventListener('click', this.removeBook);
-      this.hr = document.createElement('hr');
-      this.div.appendChild(this.hr);
+      this.removeBtn.classList.add('remove-btn')
+      this.removeBtn.innerText = 'Remove';   
+      // RemoveBtn event listener
+      this.removeBtn.addEventListener('click', this.removeBook);                                                                                                                                                   
+      // Style element
+      this.ul.style.cssText = "border-bottom: solid rgb(209, 209, 207);"
+      this.removeBtn.style.cssText = 
+        "margin-bottom: 0.5em; padding:0.3em; border: solid thin rgba(216, 72, 72, 0.2); border-radius: 0.5em;"
+      // Append elements
+      this.content.appendChild(this.span); 
+      this.ul.append(this.content, this.removeBtn);
+      this.fragment.append(this.ul);
+      this.mainContainer = document.querySelector('.main-container');
+      this.mainContainer.appendChild(this.fragment);
     }
   }
 
@@ -52,12 +56,19 @@ class Library {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const book = { title, author };
-    storedBooks = storedBooks.filter((book) => book.title !== title || book.author !== author);
-    storedBooks.push(book);
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(storedBooks));
-    storedBooks = JSON.parse(localStorage.getItem('books'));
-    window.location.reload();
+    if(!title || !author) {
+      this.error = document.querySelector('.error');
+      this.error.innerText = 'Empty field not allowed';
+      this.error.style.cssText = 'color: red; margin-top: 0.5em';
+    }
+    else{
+      storedBooks = storedBooks.filter((book) => book.title !== title || book.author !== author);
+      storedBooks.push(book);
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(storedBooks));
+      storedBooks = JSON.parse(localStorage.getItem('books'));
+      window.location.reload();
+    }  
   }
 
   addEvent() {
